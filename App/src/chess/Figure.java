2b67302bc597e2ea4fa7aed2011242480ef08708
Artem <klimenkocpp@icloud.com>
2016-12-07 19:17:42 +0600

    Fixed `findEnemy`
    It now return latest found enemy.

--- App/src/chess/Figure.java
+++ App/src/chess/Figure.java
@@ -1,9 +1,18 @@
 package chess;
 
-abstract class Figure implements Runnable {
-//    private int positionX;
-//    private int positionY;
-//   public int getX() { return positionX; }
-//    public int getY() { return positionY; }
-    
+class Figure implements Runnable {
+    public Figure(int positionX, int positionY) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+
+    private int positionX;
+    private int positionY;
+   public int getX() { return positionX; }
+    public int getY() { return positionY; }
+
+    @Override
+    public void run() {
+
+    }
 }
